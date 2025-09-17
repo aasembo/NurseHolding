ff323fa342b017ed9b6b57186e3821661319ed24
Raghu Madaan <raghu.madaan1987@gmail.com>
2025-09-17 22:01:31 +0530

    Adding https

--- config/app_okta_config.php
+++ config/app_okta_config.php
@@ -1,14 +1,14 @@
 <?php
 return [
     'App' => [
-        'fullBaseUrl' => 'http://demo.nurseholding.dataratiba.com',
+        'fullBaseUrl' => 'https://demo.nurseholding.dataratiba.com',
         'authDriver' => 'okta',
         'okta' => [
             'issuer' => 'https://integrator-1025653.okta.com/oauth2',
             'clientId' => '0oav7djsidSNarREk697',
             'clientSecret' => 'PEDe20l1aDPAJgqCcG78SiEHfMxX5GVbJvAUl4F6H2xB71hShw9qj59OEGPT2Qcl',
-            'redirectUri' => 'http://demo.nurseholding.dataratiba.com/auth/callback/',
-            'postLogoutRedirectUri' => 'http://demo.nurseholding.dataratiba.com/logout/complete',
+            'redirectUri' => 'https://demo.nurseholding.dataratiba.com/auth/callback/',
+            'postLogoutRedirectUri' => 'https://demo.nurseholding.dataratiba.com/logout/complete',
             'scopes' => ['openid', 'profile', 'email'],
             'usePkce' => true,
         ],
