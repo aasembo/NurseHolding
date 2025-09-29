d5a6fd902813d4a30716d956b45c7c228ef881c0
chetansoni1986 <chetan.it2005@gmail.com>
2025-09-29 15:10:03 +0530

    update confs

--- config/app_okta_config.php
+++ config/app_okta_config.php
@@ -1,14 +1,14 @@
 <?php
 return [
     'App' => [
-        'fullBaseUrl' => 'https://demo.nurseholding.dataratiba.com',
+        'fullBaseUrl' => 'https://prod.nurseholding.dataratiba.com',
         'authDriver' => 'okta',
         'okta' => [
             'issuer' => 'https://integrator-1025653.okta.com/oauth2',
             'clientId' => '0oav7djsidSNarREk697',
             'clientSecret' => 'PEDe20l1aDPAJgqCcG78SiEHfMxX5GVbJvAUl4F6H2xB71hShw9qj59OEGPT2Qcl',
-            'redirectUri' => 'https://demo.nurseholding.dataratiba.com/auth/callback/',
-            'postLogoutRedirectUri' => 'https://demo.nurseholding.dataratiba.com/logout/complete',
+            'redirectUri' => 'https://prod.nurseholding.dataratiba.com/auth/callback/',
+            'postLogoutRedirectUri' => 'https://prod.nurseholding.dataratiba.com/logout/complete',
             'scopes' => ['openid', 'profile', 'email'],
             'usePkce' => true,
         ],
